0e8f1fe23a0101f77e6414b61582afea55b57505
Marcus Pilkington <mtpilk@essex.ac.uk>
2017-11-08 16:02:41 +0000

    - Label added to Task Pane - Label made clickable - New Windows Form Created w/ Text Box - New Form Linked to clickable label

--- BusinessPlanWriter/TaskPaneControl.Designer.cs
+++ BusinessPlanWriter/TaskPaneControl.Designer.cs
@@ -29,28 +29,41 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Location = new System.Drawing.Point(3, -1);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(212, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Business Plan Writer";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Section 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // TaskPaneControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.AutoScroll = true;
             this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "TaskPaneControl";
-            this.Size = new System.Drawing.Size(300, 600);
+            this.Size = new System.Drawing.Size(265, 684);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +72,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- BusinessPlanWriter/TaskPaneControl.cs
+++ BusinessPlanWriter/TaskPaneControl.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Label = System.Windows.Forms.Label;
 
 namespace BusinessPlanWriter
 {
@@ -20,6 +22,19 @@ namespace BusinessPlanWriter
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                //Write pop up box code here
+                var form = new Section_1();
+                form.Show();
+                //System.Windows.Forms.MessageBox.Show("Section 1 Clicked");
+            }
         }
     }
 }
